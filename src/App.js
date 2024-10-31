85d783c68d56f7e33091790f8c79a2342b778eea
Muhammad Aurif <muhammadaurifsolangixwave@gmail.com>
2024-10-31 10:13:07 +0500

    Enhanced To-Do app design with additional styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import TodoList from './components/TodoList/TodoList';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import TodoList from './components/TodoList/TodoList';
 
 function App() {
   return (
